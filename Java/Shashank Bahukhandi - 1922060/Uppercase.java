183014711bae802bde7dce077d886759d946e937
shashankbahukhandi <shashankbahukhandi03@gmail.com>
2021-06-14 16:30:42 +0530

    Add files via upload

---
+++ Java/Shashank Bahukhandi - 1922060/Uppercase.java
@@ -0,0 +1,8 @@
+class Gfg {
+    public static void main(String args[])
+    {
+        String str = "shashank bahukhandi";
+        String gfg1 = str.toUpperCase();
+        System.out.println(gfg1);
+    }
+}
---
+++ Java/Shashank Bahukhandi - 1922060/name.java
@@ -0,0 +1,8 @@
+public class Name
+{
+    public static void main(String args[]) {
+        System.out.println("Name: Shashank Bahukhandi");
+        System.out.println("Roll No: 1922060");
+
+    }
+}
